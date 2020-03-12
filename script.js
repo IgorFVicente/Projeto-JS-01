2543fd3a354386c3fabadf523a85f5476199e814
IgorFVicente <57640592+IgorFVicente@users.noreply.github.com>
2020-03-11 21:00:46 -0300

    Change to CSS and add decimal conversions

--- script.js
+++ script.js
@@ -1,32 +1,114 @@
-var select = false
+var tipo = ""
+
+function subhex(x) {
+    if (Number(x) >= 10) {
+        switch (x) {
+            case 10:
+                x = "A"
+                break
+            case 11:
+                x = "B"
+                break
+            case 12:
+                x = "C"
+                break
+            case 13:
+                x = "D"
+                break
+            case 14:
+                x = "E"
+                break
+            case 15:
+                x = "F"
+                break
+        }
+    }
+    return x
+}
+
+function decbin(x) {
+    let potencia = 0
+    let resbin = ""
+    while (2 ** potencia < x){
+        potencia += 1
+    }
+    potencia -= 1
+    while (potencia !== -1) {
+        if (x >= 2 ** potencia) {
+            resbin += "1"
+            x -= 2 ** potencia
+        } else {
+            resbin += "0"
+        }
+        potencia -= 1
+    }
+    return resbin
+}
+
+function dechex(x) {
+    let reshex = ""
+    while (x > 16){
+        reshex += String(subhex(x % 16))
+        x = Math.trunc(x / 16)
+    }
+    reshex += String(x)
+    reshex = reshex.split('').reverse().join('')
+    return reshex
+}
 
 function converter() {
-    if (select == false) {
+    if (tipo == "") {
         alert("Por favor, selecione uma base numérica e digite um número.")
     } else {
-    let d = Number(document.getElementById("decimal").value)
+    let d = Number(document.getElementById('decimal').value)
     let b = document.getElementById('binario').value
     let h = document.getElementById('hexadecimal').value
+    switch(tipo)  {
+        case 'decimal':
+            resbin = decbin(d)
+            reshex = dechex(d)
+            document.getElementById('resdec').disabled = false
+            document.getElementById('resbin').disabled = false
+            document.getElementById('reshex').disabled = false
+            document.getElementById('resdec').value = d
+            document.getElementById('resbin').value = resbin
+            document.getElementById('reshex').value = reshex
+        break
+        case 'binario':
+        break
+        case 'hexadecimal':
+        break
+
+        let dparabin
+        let dparahex
+
+    }
     }
 }
 
 function decimal() {
-    let dec = document.getElementById('decimal').disabled = false
-    let bin = document.getElementById('binario').disabled = true
-    let hex = document.getElementById('hexadecimal').disabled = true
-    select = true
+    document.getElementById('decimal').disabled = false
+    document.getElementById('binario').disabled = true
+    document.getElementById('hexadecimal').disabled = true
+    document.getElementById('binario').value = ""
+    document.getElementById('hexadecimal').value = "" 
+    tipo = "decimal"
 }
 
 function binario() {
-    let dec = document.getElementById('decimal').disabled = true
-    let bin = document.getElementById('binario').disabled = false
-    let hex = document.getElementById('hexadecimal').disabled = true
-    select = true
+    document.getElementById('decimal').disabled = true
+    document.getElementById('binario').disabled = false
+    document.getElementById('hexadecimal').disabled = true
+    document.getElementById('decimal').value = ""
+    document.getElementById('hexadecimal').value = "" 
+    tipo = "binario"
 }
 
 function hexadecimal() {
-    let dec = document.getElementById('decimal').disabled = true
-    let bin = document.getElementById('binario').disabled = true
-    let hex = document.getElementById('hexadecimal').disabled = false
-    select = true
+    document.getElementById('decimal').disabled = true
+    document.getElementById('binario').disabled = true
+    document.getElementById('hexadecimal').disabled = false
+    document.getElementById('binario').value = ""
+    document.getElementById('decimal').value = "" 
+    tipo = "hexadecimal"
 }
